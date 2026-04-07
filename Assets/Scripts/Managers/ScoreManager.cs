f5a03d1ddb1319994e5e0db2d22b6dffd9770848
jinjooyoung <jjy26297449@gmail.com>
2026-04-07 14:31:33 +0900

    [Feat] ScoreManager 점수 증감, 콤보 점수 반환 함수 작성

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,33 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("현재 점수")]
+    int score;
+
+    [Header("콤보 점수")]
+    int[] comboScore = { 10, 15, 20, 25, 30 };
+
+    void Awake()
+    {
+        score = 0;
+    }
+
+    public void AddScore(int score)
+    {
+        this.score += score;
+    }
+
+    public int GetComboBonus(int combo)
+    {
+        if (combo < 0 || combo > 5) return 0;
+
+        return comboScore[combo - 1];
+    }
+
+    public int GetCurrentScore()
+    {
+        return score;
+    }
+}
