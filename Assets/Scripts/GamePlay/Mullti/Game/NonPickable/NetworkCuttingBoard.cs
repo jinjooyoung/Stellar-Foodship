3e523a0eb823231407673c6629a7f89a45263298
jinjooyoung <jjy26297449@gmail.com>
2026-06-09 22:52:29 +0900

    [Feat] 도마 멀티 버전 +(타이머 제거, 연타 방식으로 변경)

---
+++ Assets/Scripts/GamePlay/Mullti/Game/NonPickable/NetworkCuttingBoard.cs
@@ -0,0 +1,89 @@
+using Fusion;
+using UnityEngine;
+
+public class NetworkCuttingBoard : NewNonPickable
+{
+    [Header("Cutting")]
+    [Networked]
+    public int CutProgress { get; set; }
+
+    public override bool CanPlace => true;
+
+    //================================================
+
+    public override void Interact(NewPlayer player)
+    {
+        if (CutProgress > 0 && CutProgress < 100) return;
+
+        base.Interact(player);
+    }
+
+    // 우클릭
+    public override void InteractSecondary(NewPlayer player)
+    {
+        if (!Object.HasStateAuthority)
+            return;
+
+        if (HeldItem == null)
+            return;
+
+        NetworkIngredient ingredient =
+            HeldItem.GetComponent<NetworkIngredient>();
+
+        if (ingredient == null)
+            return;
+
+        // 썰 수 없는 재료
+        if (!ingredient.ingredientData.isCutable)
+            return;
+
+        // 이미 썰림
+        if (ingredient.IsCut)
+            return;
+
+        CutProgress += 20;
+
+        if (CutProgress >= 100)
+        {
+            CutProgress = 100;
+
+            ingredient.OnCutComplete();
+
+            Debug.Log("손질 완료");
+        }
+    }
+
+    //================================================
+
+    protected override bool TakeItem(NewPlayer player)
+    {
+        // 써는 중에는 못 집음
+        if (CutProgress > 0 &&
+            CutProgress < 100)
+        {
+            Debug.Log("손질 중인 재료입니다.");
+            return false;
+        }
+
+        return base.TakeItem(player);
+    }
+
+    //================================================
+
+    protected override void OnItemPlaced(NewPickable item)
+    {
+        CutProgress = 0;
+    }
+
+    protected override void OnItemTaken(NewPickable item)
+    {
+        CutProgress = 0;
+    }
+
+    //================================================
+
+    public float GetNormalizedProgress()
+    {
+        return CutProgress / 100f;
+    }
+}
